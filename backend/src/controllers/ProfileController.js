9a8322bae81533fab02302c41ebda064438bd943
wsdrocha <wsdrocha.damasceno@gmail.com>
2020-03-25 12:19:55 -0300

    Backend da App

---
+++ backend/src/controllers/ProfileController.js
@@ -0,0 +1,15 @@
+const connection = require('../database/connection');
+
+module.exports = {
+  async index(request, response){
+    const ong_id = request.headers.authorization;
+
+    incidents = await connection('incidents')
+    .where('ong_id', ong_id)
+    .select('*');
+
+    return response.json(incidents);
+  
+  }
+    
+}
